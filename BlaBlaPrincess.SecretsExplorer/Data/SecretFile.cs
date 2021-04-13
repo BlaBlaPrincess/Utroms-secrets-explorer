1b89f013b08e55a579a7c95353f2cf3ba556b3cd
Alexander Simakin <qq.siiimka@gmail.com>
2021-04-14 00:07:01 +1000

    Added SecretFile class

---
+++ BlaBlaPrincess.SecretsExplorer/Data/SecretFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BlaBlaPrincess.SecretsExplorer.Data
+{
+    public class SecretFile : ISecret
+    {
+        public string Name { get; set; }
+        public int Weight { get; }
+
+        public SecretFile(string name, int weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public bool Equals(SecretFile file)
+        {
+            return Name == file.Name &&
+                   Weight == file.Weight;
+        }
+        
+        public override bool Equals(object obj)
+        {
+            return obj is SecretFile file && Equals(file);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Weight);
+        }
+    }
+}
