0846468ff0e9b44d0ce6906936e2dceda55754df
Alexander Simakin <qq.siiimka@gmail.com>
2021-04-14 00:20:05 +1000

    Added ISecretsExplorer interface

---
+++ BlaBlaPrincess.SecretsExplorer/Business/ISecretsExplorer.cs
@@ -0,0 +1,8 @@
+﻿namespace BlaBlaPrincess.SecretsExplorer.Business
+{
+    public interface ISecretsExplorer
+    {
+        public void ExploreSecrets();
+        public void ExploreSecrets(string source);
+    }
+}
