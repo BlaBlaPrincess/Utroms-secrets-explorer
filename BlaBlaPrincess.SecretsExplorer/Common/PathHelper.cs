28fcdde3a9642dfae9a01744aa234ca10175c648
Alexander Simakin <qq.siiimka@gmail.com>
2021-04-14 00:23:01 +1000

    Added PathHelper class with GetFileOrDirectoryName method

---
+++ BlaBlaPrincess.SecretsExplorer/Common/PathHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace BlaBlaPrincess.SecretsExplorer.Common
+{
+    public static class PathHelper
+    {
+        public static string GetFileOrDirectoryName(string path)
+        {
+            return Path.GetFileName(Path.TrimEndingDirectorySeparator(path));
+        }
+    }
+}
