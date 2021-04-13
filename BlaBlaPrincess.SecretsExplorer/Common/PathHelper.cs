220c3a8df6b36dd3fbfad15b63478c056ec0a89f
Alexander Simakin <qq.siiimka@gmail.com>
2021-04-14 00:47:51 +1000

    Added GetParentName, RenameFile, AddIdentifier methods for PathHelper class

--- BlaBlaPrincess.SecretsExplorer/Common/PathHelper.cs
+++ BlaBlaPrincess.SecretsExplorer/Common/PathHelper.cs
@@ -8,5 +8,28 @@ namespace BlaBlaPrincess.SecretsExplorer.Common
         {
             return Path.GetFileName(Path.TrimEndingDirectorySeparator(path));
         }
+
+        public static string GetParentName(string path)
+        {
+            return Path.GetDirectoryName(Path.TrimEndingDirectorySeparator(path));
+        }
+        
+        public static string RenameFile(string path, string name)
+        {
+            var dir = GetParentName(path);
+            return Path.Join(dir, name);
+        }
+        
+        public static string AddIdentifier(string path, int id)
+        {
+            if (id == 0)
+            {
+                return path;
+            }
+
+            var ex = Path.GetExtension(path);
+            var name = Path.GetFileNameWithoutExtension(Path.TrimEndingDirectorySeparator(path));
+            return RenameFile(path, $"{name} ({id}){ex}");
+        }
     }
 }
