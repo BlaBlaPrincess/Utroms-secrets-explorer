b7bb976ccb1ffe27cd53bb39c2408df54f005c2b
Alexander Simakin <qq.siiimka@gmail.com>
2021-05-02 14:25:45 +1000

    UtromsSecretSizeHelper class interface simplified to better fit the assignment

--- BlaBlaPrincess.SecretsExplorer/Common/UtromsSecretSizeHelper.cs
+++ BlaBlaPrincess.SecretsExplorer/Common/UtromsSecretSizeHelper.cs
@@ -17,11 +17,5 @@ namespace BlaBlaPrincess.SecretsExplorer.Common
                 throw new FormatException("An unexpected secret format.");
             }
         }
-
-        public static bool TryToGetSecretSize(string path, out int weight)
-        {
-            var sWeight = File.ReadAllText(path).Replace("Kb", string.Empty);
-            return int.TryParse(sWeight, out weight);
-        }
     }
 }
